35dcef99e96b9a0b19a08409ca64e9de6a5c1b8d
borbor <boris00043@gmail.com>
2016-01-10 12:17:45 +0300

    документация
    добавлена документация на русском языке

--- CSVReader.php
+++ CSVReader.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * класс для чтения csv файлов с произвольным разделителем
  * Created by PhpStorm.
  * User: boris
  * Date: 01.01.16
@@ -8,21 +9,34 @@
  */
 class CSVReader{
 	private $file;
+	private $separator;
 
 	/**
 	 * CSVReader constructor.
 	 *
-	 * @param $filename
+	 * @param string $filename
+	 * путь к csv файлу
+	 * @param string $separator
+	 * разделитель csv файлов
 	 */
-	function CSVReader($filename){
+	function CSVReader($filename, $separator=","){
 		$this->file=@file($filename);
+		$this->separator=$separator;
+		return $this->getArray();
 	}
+
 	private function parseString($s){
-		return preg_split("/,/",$s);
+		return preg_split("/".$this->separator."/",$s);
 	}
+
 	private function getLine($n){
 		return $this->parseString($this->file[$n]);
 	}
+
+	/**
+	 * конвертирует csv файл в массив php
+	 * @return array
+	 */
 	public function getArray(){
 		$arr=[];
 		for($i=0;$i<count($this->file);$i++){
