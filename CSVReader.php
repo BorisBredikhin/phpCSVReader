e481a93b34a9445a53ec28d872a909a5bcf9784a
borbor <boris00043@gmail.com>
2016-01-10 12:18:35 +0300

    Revert "документация"
    This reverts commit f202a7b9b092eedd69174ce47c2e9024780ef806.

--- CSVReader.php
+++ CSVReader.php
@@ -1,7 +1,6 @@
 <?php
 
 /**
- * класс для чтения csv файлов с произвольным разделителем
  * Created by PhpStorm.
  * User: boris
  * Date: 01.01.16
@@ -9,34 +8,21 @@
  */
 class CSVReader{
 	private $file;
-	private $separator;
 
 	/**
 	 * CSVReader constructor.
 	 *
-	 * @param string $filename
-	 * путь к csv файлу
-	 * @param string $separator
-	 * разделитель csv файлов
+	 * @param $filename
 	 */
-	function CSVReader($filename, $separator=","){
+	function CSVReader($filename){
 		$this->file=@file($filename);
-		$this->separator=$separator;
-		return $this->getArray();
 	}
-
 	private function parseString($s){
-		return preg_split("/".$this->separator."/",$s);
+		return preg_split("/,/",$s);
 	}
-
 	private function getLine($n){
 		return $this->parseString($this->file[$n]);
 	}
-
-	/**
-	 * конвертирует csv файл в массив php
-	 * @return array
-	 */
 	public function getArray(){
 		$arr=[];
 		for($i=0;$i<count($this->file);$i++){
